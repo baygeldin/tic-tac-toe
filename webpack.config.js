155ae1b3734296abe0b7ea3a39e94ddb6215117a
Alexander Baygeldin <a.baygeldin@gmail.com>
2017-04-11 08:02:21 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,67 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const PROD = process.env.NODE_ENV === 'production'
+const DEV_PORT = 9000
 
 let plugins = [
-  new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
-  new webpack.NoEmitOnErrorsPlugin()
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NamedModulesPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
 ]
 
-// Minify in production.
-if (PROD) config.plugins.push(new webpack.optimize.UglifyJsPlugin())
+let styleLoader
+
+if (PROD) {
+  plugins = plugins.concat([
+    // Minify in production.
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin({ filename: 'style.css', allChunks: true })
+  ])
+
+  styleLoader = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 1,
+          minimize: true
+        }
+      },
+      'postcss-loader'
+    ]
+  })
+} else {
+  styleLoader = [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: true,
+        importLoaders: 1,
+        localIdentName: '[path][name]__[local]--[hash:base64:5]'
+      }
+    },
+    'postcss-loader'
+  ]
+}
 
 module.exports = {
-  entry: path.resolve(__dirname, 'client/index.jsx'),
+  entry: [
+    'react-hot-loader/patch',
+    `webpack-dev-server/client?http://localhost:${DEV_PORT}`,
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'client/index.jsx')
+  ],
 
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'static/dist'),
-    filename: 'bundle.js'
+    publicPath: '/dist/'
   },
 
   module: {
@@ -29,19 +75,11 @@ module.exports = {
       exclude: path.resolve(__dirname, 'node_modules')
     }, {
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      }),
+      use: styleLoader,
+      exclude: path.resolve(__dirname, 'node_modules')
+    }, {
+      test: /\.(svg|png|gif|jpg|wav|otf|ttf)$/,
+      use: 'file-loader?name=[name].[ext]&publicPath=/dist/&outputPath=assets/',
       exclude: path.resolve(__dirname, 'node_modules')
     }]
   },
@@ -50,7 +88,9 @@ module.exports = {
 
   resolve: {
     alias: {
-      components: path.resolve(__dirname, 'client/components')
+      components: path.resolve(__dirname, 'client/components'),
+      constants: path.resolve(__dirname, 'client/constants.js'),
+      debugger: path.resolve(__dirname, 'client/debug.js')
     },
     extensions: ['.jsx', '.js', '.json']
   },
@@ -59,9 +99,18 @@ module.exports = {
     'socket.io': 'io'
   },
 
-  devtool: 'source-map',
+  devtool: PROD ? false : 'inline-source-map',
 
   watchOptions: {
     aggregateTimeout: 500
+  },
+
+  devServer: {
+    hot: true,
+    stats: 'minimal',
+    port: DEV_PORT,
+    host: '0.0.0.0',
+    contentBase: path.resolve(__dirname, 'static/dist'),
+    publicPath: '/dist/'
   }
 }
